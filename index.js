7546d6620a8a5e0d245123a6ba86ef49d2f9e6e3
Matthew <matthew.stewart827@gmail.com>
2023-08-01 18:08:22 -0500

    complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = 31 * 2
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1
+
+let num3 = 10
+let num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(1,2,3,4,20)
